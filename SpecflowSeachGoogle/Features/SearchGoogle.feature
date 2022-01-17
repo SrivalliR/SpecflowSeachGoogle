1c4ef7dc6ef9eebc8166cf91aae977fde089ad2c
Rahini <Rahini@LAPTOP-V0LLDNHG>
2022-01-17 18:01:20 +0530

    Add project files.

---
+++ SpecflowSeachGoogle/Features/SearchGoogle.feature
@@ -0,0 +1,16 @@
+﻿Feature: SearchGoogle
+
+@mytag
+Scenario Outline: Search a string in Google
+	Given I navigate to Google page
+	And Enter <searchstring> in the searchbox
+	| <searchstring> |
+	| searchstring    |
+	When Press search button
+	Then the result page should be displayed
+
+Examples:
+	| searchstring |
+	| Selenium     |
+	| BDD          |
+	| NUNIT        |
---
+++ SpecflowSeachGoogle/Steps/GoogleSearch.cs
@@ -0,0 +1,73 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using System;
+using NUnit.Framework;
+
+namespace SpecflowSeachGoogle.Steps
+{
+    [Binding]
+    public sealed class GoogleSearch
+    {
+        IWebDriver driver;
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        //private readonly ScenarioContext _scenarioContext;
+
+        //public GoogleSearch(ScenarioContext scenarioContext)
+        //{
+        //    _scenarioContext = scenarioContext;
+        //}
+
+        [Given(@"I navigate to Google page")]
+        public void GivenINavigateToGooglePage()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+        }
+
+        [Given(@"Enter Selenium in the searchbox")]
+        public void GivenEnterSeleniumInTheSearchbox(Table table)
+        {
+            var data = table.CreateInstance<>
+                driver.FindElement(By.Name("q")).SendKeys(table.AddRow);
+        }
+
+
+
+        [Given(@"Enter string in the searchbox")]
+        public void GivenEnterStringInTheSearchbox()
+        {
+            driver.FindElement(By.Name("q")).SendKeys("Selenium");
+        }
+
+        [When(@"Press search button")]
+        public void WhenPressSearchButton()
+        {
+            driver.FindElement(By.Name("btnK")).Click();
+        }
+
+        [Then(@"the result page should be displayed")]
+        public void ThenTheResultPageShouldBeDisplayed()
+        {
+            //bool value1;
+            string actualstring = "Selenium";
+            var handle1= driver.WindowHandles[1];
+            string handle1Title=driver.SwitchTo().Window(handle1).Title;
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            if (actualstring.ToLower() == handle1Title.ToLower())
+            {
+                Console.WriteLine("assertion done Equql");
+            }
+            else
+            {
+                //Assert.That(actualstring.ToLower(), Is.EqualTo(handle1Title.ToLower()));
+                Console.WriteLine("assertion done Notequal ");
+            }
+        }
+
+    }
+}
